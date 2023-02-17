b5436b25b8b4437a423195a69786c6753d02f15e
Hardik Savani <itsolutionstuff@gmail.com>
2023-02-17 10:11:56 +0530

    first commit

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 10 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="container">
+    @yield('content')
+</div>
+    
+</body>
+</html>
